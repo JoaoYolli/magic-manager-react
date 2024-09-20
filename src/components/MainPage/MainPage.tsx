211e256088c69364811b4c3367a205303c71f824
JoaoGabrielPaiva <yoaojoao@gmail.com>
2024-09-20 22:36:09 +0200

    Home Created

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -8,53 +8,98 @@ import CardModel from '../../models/card';
 function MainPage() {
   const [cardList, setCardsList] = useState<CardModel[]>([]); // Initialize as an empty array
   const [isHovering, setIsHovering] = useState<boolean>(false); // New state for hover
+  const [searchingCommander, setSearchingCommander] = useState<boolean>(false); // State for Commander checkbox
   const navigate = useNavigate();
-  
+
   const getItemFromLocalStorage = (key: string): string | null => {
     return localStorage.getItem(key);
   };
 
-  async function searchRandCard(){
-    fetch('https://api.scryfall.com/cards/random', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(async response => {
-      if (!response.ok) {
-        alert("Scryfall no funciona, no es mi culpa");
-      } else {
-        let respuesta = await response.json();
-        let cards: CardModel[] = [];
+  async function searchRandCard() {
+    if (searchingCommander) {
+      
+      fetch('http://localhost:6173/randCommander', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(async (response) => {
+          if (!response.ok) {
+            alert('Error en el servidor');
+          } else {
+            let respuesta = await response.json();
+            let cards: CardModel[] = [];
 
-        if (Array.isArray(respuesta)) {
-          respuesta.forEach((card: { name: string; image_uris: { normal: string; }; scryfall_uri: string}) => {
-            let carta: CardModel = {
-              name: card.name,
-              imgUrl: card.image_uris?.normal || '',
-              scryfallUrl: card.scryfall_uri || ''
-            };
-            cards.push(carta);
-          });
-        } else {
-          let carta: CardModel = {
-            name: respuesta.name,
-            imgUrl: respuesta.image_uris?.normal || '',
-            scryfallUrl: respuesta.scryfall_uri || ''
-          };
-          cards.push(carta);
-        }
+            if (Array.isArray(respuesta)) {
+              respuesta.forEach((card: { name: string; image_uris: { normal: string }; scryfall_uri: string }) => {
+                let carta: CardModel = {
+                  name: card.name,
+                  imgUrl: card.image_uris?.normal || '',
+                  scryfallUrl: card.scryfall_uri || '',
+                };
+                cards.push(carta);
+              });
+            } else {
+              respuesta = respuesta.respuesta;
+              let carta: CardModel = {
+                name: respuesta.name,
+                imgUrl: respuesta.imgUrl || '',
+                scryfallUrl: respuesta.scryfallUrl || '',
+              };
+              cards.push(carta);
+            }
+            // setCardsList((prevCards) => [...cards, ...prevCards]); // UPDATE
+            setCardsList(cards);
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
 
-        setCardsList(cards); 
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+    } else {
+      fetch('http://localhost:6173/randCard', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(async (response) => {
+          if (!response.ok) {
+            alert('Error en el servidor');
+          } else {
+            let respuesta = await response.json();
+            let cards: CardModel[] = [];
+
+            if (Array.isArray(respuesta)) {
+              respuesta.forEach((card: { name: string; image_uris: { normal: string }; scryfall_uri: string }) => {
+                let carta: CardModel = {
+                  name: card.name,
+                  imgUrl: card.image_uris?.normal || '',
+                  scryfallUrl: card.scryfall_uri || '',
+                };
+                cards.push(carta);
+              });
+            } else {
+              respuesta = respuesta.respuesta;
+              let carta: CardModel = {
+                name: respuesta.name,
+                imgUrl: respuesta.imgUrl || '',
+                scryfallUrl: respuesta.scryfallUrl || '',
+              };
+              cards.push(carta);
+            }
+            // setCardsList((prevCards) => [...cards, ...prevCards]); // UPDATE
+            setCardsList(cards);
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
   }
 
-  async function init(){
+  async function init() {
     try {
       interface PostData {
         token: string;
@@ -63,22 +108,22 @@ function MainPage() {
       const accessToken = getItemFromLocalStorage('accessToken') || '';
 
       let data: PostData = {
-        token: accessToken
+        token: accessToken,
       };
 
       const response = await fetch(`${config.srvURL}:${config.serverPort}/verify-token`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       });
 
       if (response.status === 200) {
         const responseData = await response.json();
         sessionStorage.setItem('user', responseData.mail);
       } else {
-        localStorage.setItem("accessToken", "");
+        localStorage.setItem('accessToken', '');
         navigate('/');
       }
     } catch (error) {
@@ -97,15 +142,21 @@ function MainPage() {
     }, 20000); // 20 segundos = 20000 milisegundos
 
     return () => clearInterval(intervalo);
-  }, [isHovering]); // Depend on isHovering
+  }, [isHovering, searchingCommander]); // Depend on both isHovering and searchingCommander
 
+  //Devuelve el html
   return (
     <>
-      {cardList.map((card, index) => (
-        <CardComponent 
-          key={index} 
-          card={card}
+      <div>
+        <input
+          type="checkbox"
+          checked={searchingCommander}
+          onChange={(e) => setSearchingCommander(e.target.checked)}
         />
+        <label htmlFor="">Commander</label>
+      </div>
+      {cardList.map((card, index) => (
+        <CardComponent key={index} card={card} />
       ))}
     </>
   );
---
+++ src/components/ModalFullScreen/ModalFullScreen.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ModalFullScreen.css'
+
+function ModalFullScreen() {
+
+    return (
+        <div className="modal-dialog modal-fullscreen-sm-down">
+            <iframe src="" frameBorder="0"></iframe>
+        </div>
+    );
+}
+
+export default ModalFullScreen;
